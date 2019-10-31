e6b6657550a0a79d2e70c0ff6ac29a7214053561
Munir <munir@tuahpacket.net>
2019-10-31 11:10:06 +0800

    setup

---
+++ src/fsktm/fop/Main.java
@@ -0,0 +1,17 @@
+package fsktm.fop;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello World!");
+
+        function(1);
+    }
+
+    static void function(int x) {
+        System.out.println(x);
+    }
+
+
+}
